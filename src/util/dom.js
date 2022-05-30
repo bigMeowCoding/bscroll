7b9da8c6ae4a19bd37c2fea55fac8a30c3006079
zhouyijun <zhou495248579@hotmail.com>
2022-05-31 00:20:59 +0800

    style:format

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,68 +1,68 @@
 export function addEvent(el, type, fn, capture) {
-  el.addEventListener(type, fn, !!capture);
+    el.addEventListener(type, fn, !!capture);
 }
 export function removeEvent(el, type, fn, capture) {
-  el.removeEventListener(type, fn, !!capture);
+    el.removeEventListener(type, fn, !!capture);
 }
 const elStyle = document.createElement("div").style;
 const vendor = (function () {
-  let transformNames = {
-    webkit: "webkitTransform",
-    Moz: "MozTransform",
-    O: "OTransform",
-    ms: "msTransform",
-    standard: "transform",
-  };
-  for (let k of Object.keys(transformNames)) {
-    if (elStyle[transformNames[k]] !== undefined) {
-      return k;
+    let transformNames = {
+        webkit: "webkitTransform",
+        Moz: "MozTransform",
+        O: "OTransform",
+        ms: "msTransform",
+        standard: "transform",
+    };
+    for (let k of Object.keys(transformNames)) {
+        if (elStyle[transformNames[k]] !== undefined) {
+            return k;
+        }
     }
-  }
-  return false;
+    return false;
 })();
 export function prefixStyle(style) {
-  if (vendor === false) {
-    return false;
-  }
-  if (vendor === "standard") {
-    return style;
-  }
-  return vendor + style.charAt(0).toUpperCase() + style.slice(1);
+    if (vendor === false) {
+        return false;
+    }
+    if (vendor === "standard") {
+        return style;
+    }
+    return vendor + style.charAt(0).toUpperCase() + style.slice(1);
 }
 
 let transform = prefixStyle("transform");
 export const hasTouch = "ontouchstart" in window;
 
 export const style = {
-  transform,
-  transitionTimingFunction: prefixStyle("transitionTimingFunction"),
-  transitionDuration: prefixStyle("transitionDuration"),
-  transitionDelay: prefixStyle("transitionDelay"),
-  transformOrigin: prefixStyle("transformOrigin"),
-  transitionEnd: prefixStyle("transitionEnd"),
+    transform,
+    transitionTimingFunction: prefixStyle("transitionTimingFunction"),
+    transitionDuration: prefixStyle("transitionDuration"),
+    transitionDelay: prefixStyle("transitionDelay"),
+    transformOrigin: prefixStyle("transformOrigin"),
+    transitionEnd: prefixStyle("transitionEnd"),
 };
 export const hasPerspective = prefixStyle("perspective") in elStyle;
 
 const TOUCH_EVENT = 1;
 const MOUSE_EVENT = 2;
 export const eventType = {
-  touchstart: TOUCH_EVENT,
-  touchmove: TOUCH_EVENT,
-  touchend: TOUCH_EVENT,
+    touchstart: TOUCH_EVENT,
+    touchmove: TOUCH_EVENT,
+    touchend: TOUCH_EVENT,
 
-  mousedown: MOUSE_EVENT,
-  mousemove: MOUSE_EVENT,
-  mouseup: MOUSE_EVENT,
+    mousedown: MOUSE_EVENT,
+    mousemove: MOUSE_EVENT,
+    mouseup: MOUSE_EVENT,
 };
 export function offset(el) {
-  let left, top;
-  while (el) {
-    left -= el.offsetLeft; // TODO 不明白为什么用-
-    top -= el.offsetTop;
-    el = el.offsetParent;
-  }
-  return {
-    left,
-    top,
-  };
+    let left, top;
+    while (el) {
+        left -= el.offsetLeft; // TODO 不明白为什么用-
+        top -= el.offsetTop;
+        el = el.offsetParent;
+    }
+    return {
+        left,
+        top,
+    };
 }
