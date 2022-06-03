60b1e8f6a751b3f031ca3931f014ecd4d2d1ecad
zhouyijun <zhou495248579@hotmail.com>
2022-06-03 21:06:23 +0800

    chore:增加env

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 var path = require("path");
 var webpack = require("webpack");
 var version = require("./package.json").version;
-
+const mode = process.env.NODE_ENV === "prod" ? "production" : "development";
 module.exports = {
-  mode: "development",
-  entry: "./src/index",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bscroll.js",
-    library: "BScroll",
-    libraryTarget: "umd",
-    publicPath: "/assets/",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "demo"),
+    mode,
+    entry: "./src/index",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
     },
-    compress: true,
-    port: 9000,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __VERSION__: JSON.stringify(version),
-    }),
-  ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    target: ["web"],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "docs/slider"),
+        },
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VERSION__: JSON.stringify(version),
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bscroll.js",
+        library: {
+            name: "BScroll",
+            type: "umd",
+            export: "default",
+        },
+        publicPath: "/assets/",
+    },
 };
